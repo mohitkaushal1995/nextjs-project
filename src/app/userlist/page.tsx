56fab47d7f2db022a2145a94744294b8bc0b5b7f
mohitkaushal1995 <mohitk@ideafoundation.in>
2023-11-26 18:06:17 +0530

    my nextjs project first commit

--- src/app/userlist/page.tsx
+++ src/app/userlist/page.tsx
@@ -1,8 +1,8 @@
 // @ts-nocheck
 async function getUsers(){
    const res = await fetch("http://localhost:3000/api/users") 
- const  data = await res.json()
-   return data.data
+ const  { data } = await res.json()
+   return data
 
 }
 
